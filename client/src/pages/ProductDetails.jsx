f4fe890a3f9d6f523047bab7f55db5f4e88e370a
aiswarya s <aiswaryas4906@gmail.com>
2025-07-15 16:51:52 +0530

    image

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -82,13 +82,13 @@ function ProductDetails() {
         <div className='min-h-screen flex flex-col md:flex-row p-2 justify-center items-center gap-5 bg-blue-50 mt-21'>
 
             <div id="first-section" className=' max-w-[600px] overflow-hidden '>
-                <img src={product.image} alt="product-img" className='h-90 md:h-120 md:w-300  object-contain'/>
+                <img src={product.image} alt="product-img" className='h-90 md:h-120 md:w-[300px] object-contain'/>
                 <div id='buttons' className='flex flex-row items-center justify-center gap-2 p-2 font-bold '>
                     <button className='p-2 bg-amber-400 w-full rounded text-white h-10 text-xs lg:text-lg ' onClick={addToCart}>ADD TO CART</button>
                     <button onClick={()=>handleBuyNow(product)} className='w-full bg-amber-500 rounded p-2 text-white h-10 text-xs lg:text-lg'>BUY NOW</button>
                 </div>
             </div>
-            <div id='details' className='flex flex-col gap-2 items-start justify-start p-1 sm:w-300 '>
+            <div id='details' className='flex flex-col gap-2 items-start justify-start p-1 min-w-200 sm:w-300 '>
                 <p className='text-md text-gray-600 font-bold'>{product.name}</p>
                 <p className='text-sm text-gray-500 '>{product.brand}</p>
                 <p className='text-black text-lg font-bold'>₹{product.price}</p>
