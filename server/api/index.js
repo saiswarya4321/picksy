61eb7253750e606e14aae4011706f536d3ee0427
aiswarya s <aiswaryas4906@gmail.com>
2025-07-16 15:37:01 +0530

    index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,25 +1,21 @@
 const express = require("express");
 const { connectionDB } = require("../config/db.js");
-const userRouter = require("../routes/userRouter.js");
 require("dotenv").config();
-const cookie = require("cookie-parser");
+
+const userRouter = require("../routes/userRouter.js");
 const otprouter = require("../routes/otpRouter.js");
-const cors = require("cors");
 const productrouter = require("../routes/productRouter.js");
 const cartRouter = require("../routes/cartRouter.js");
 const paymentrouter = require("../routes/payment.js");
 const orderRouter = require("../routes/orderRouter.js");
 const paymentnewrouter = require("../routes/paymentnew.js");
+
+const cookie = require("cookie-parser");
+const cors = require("cors");
 const serverless = require("serverless-http");
 
 const app = express();
 
-// Connect to MongoDB once before exporting
-(async () => {
-  await connectionDB();
-})();
-
-// Middleware
 app.use(express.json());
 app.use(cookie());
 
@@ -44,9 +40,21 @@ app.use("/order", orderRouter);
 app.use("/paymentone", paymentnewrouter);
 
 app.get("/", (req, res) => {
-  res.send("Welcome to picksy");
+  res.send("Welcome to Picksy");
 });
 
+// ❌ Do NOT use app.listen()
 
-module.exports = app;
-module.exports.handler = serverless(app);
+// ✅ Export only after DB connection
+let isExported = false;
+
+const setupAndExport = async () => {
+  await connectionDB();
+  if (!isExported) {
+    isExported = true;
+    module.exports = app;
+    module.exports.handler = serverless(app);
+  }
+};
+
+setupAndExport();
